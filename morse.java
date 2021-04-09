4ef4bd1ae5e8ed96ad34467beb177c32b1f1f478
tar-telperien <venicapiea@verizon.net>
2021-04-09 12:44:27 -0500

    Create morse class to encode and decode data strings

---
+++ morse.java
@@ -0,0 +1,37 @@
+public class morse {
+    /* A = dit daw
+       B = daw dit dit dit
+       C = daw dit daw dit
+       D = 
+       E = 
+       F = 
+       G = 
+       H = 
+       I = 
+       J = 
+       K = 
+       L = 
+       M = 
+       N = 
+       O = 
+       P = 
+       Q = 
+       R = 
+       S = 
+       T = 
+       U = 
+       V = 
+       W = 
+       X = 
+       Y = 
+       Z =
+
+       . = blue
+       - = red
+       next letter/letter break = green
+       next word = white
+    */
+
+
+
+}
